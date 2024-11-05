4c26b968cfaa9cbee1c327ef32288895acc42573
karol <karolostrowski111@gmail.com>
2024-11-06 00:01:45 +0100

    done some work

--- pub-crawler.py
+++ pub-crawler.py
@@ -5,120 +5,90 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from pynput.keyboard import Key, Controller
 from urllib.parse import urlparse
+from random import uniform
+import pandas as pd
 import time
+import pyperclip
 
 """
 hallucinogens OR psychedelics OR psychedelic OR "lysergic acid diethylamide" OR psilocybin OR dimethyltryptamine OR "5-MeO-DMT" OR mescaline OR ayahuasca OR LSD) AND (("purpose in life" AND "PILS") OR ("meaning in life" AND "MLQ"
 """
 #wymyslic cos ze jak sie wysypie to zeby nie trzeba bylo od poczatku leciec
-def scrape_search_results(path):
-    directory = Path(path / "scraped-htmls")
-    directory.mkdir(exist_ok=True)
+def scrape_search_results(driver, query, page_numer):
+    #TODO
+    #process seach query so it can be pasted into url
+    url = f"https://scholar.google.com/scholar?start={page_numer}0&q={query}&hl=pl&as_sdt=0,5"
+    driver.get(url)
+    html_source = driver.page_source
 
-    return directory
+    return html_source
 
-def gather_urls(path):
-    
+def gather_urls(scraped_html):
     urls = set()
-
-    for _, html_file in enumerate(list(path.glob("*.html"))):
-        with open(html_file, "r", encoding="utf-8") as file:
-            html_content = file.read()
-        soup = BeautifulSoup(html_content, "html.parser")
-
-        for url in soup.find_all("a", href=True):
-            if ("https" in url["href"] and 
-                "google" not in url["href"] and 
-                "pdf" not in url["href"]):
-
-                urls.add(url["href"])
+    soup = BeautifulSoup(scraped_html, "html.parser")
+    for url in soup.find_all("a", href=True):
+        if ("https" in url["href"] and 
+            "google" not in url["href"] and 
+            "pdf" not in url["href"]):
+            urls.add(url["href"])
 
     return urls
 
-def get_contents(url, driver):
+def get_contents(url, driver):                          
+    driver.get(url)
+    time.sleep(uniform(2, 3))
+
+    try:                                                        #to fajnie jakby bylo sczytywane z jakiegos pliku a nie bylo hardcoded
+        accept_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Accept all cookies') or contains(text(), 'Accept All Cookies') or contains(text(), 'I Accept') or contains(text(), 'Accept Cookies')]")
+        accept_button.click()
+    except NoSuchElementException:
+        print("no cookies button found")
+
+    body = driver.find_element("tag name", "body")
+    body.send_keys(Keys.CONTROL, 'a')
+    time.sleep(uniform(0.2, 0.3))
+
+    body.send_keys(Keys.CONTROL, 'c')
+    time.sleep(uniform(0.1, 0.2))      
     
-    keyboard = Controller()
-                          
-    try:
-        driver.get('chrome://settings/content/clipboard')
-        time.sleep(2)
-        body = driver.find_element("tag name", "body")
-        for i in range(4):
-            body.send_keys(Keys.TAB)
-            time.sleep(0.1)
-        time.sleep(0.1)
-        keyboard.press(Key.enter)
-        keyboard.release(Key.enter)
-        time.sleep(0.1)
-        keyboard.press(Key.tab)
-        keyboard.release(Key.tab)
-        time.sleep(0.1)
-        parsed_url = urlparse(url)
-        if ("www.ncbi.nlm.nih.gov" not in parsed_url or
-            "https://search.proquest.com/" not in parsed_url):
-            root_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
-        elif "www.ncbi.nlm.nih.gov" in parsed_url:
-            root_url = "https://pmc.ncbi.nlm.nih.gov/"
-        elif "https://search.proquest.com/" in parsed_url:
-            root_url = "https://proquest.com/"
-        keyboard.type(root_url)
-        time.sleep(0.1)
-        keyboard.press(Key.tab)
-        keyboard.release(Key.tab)
-        time.sleep(0.1)
-        keyboard.press(Key.tab)
-        keyboard.release(Key.tab)
-        time.sleep(0.1)
-        keyboard.press(Key.enter)
-        keyboard.release(Key.enter)
-        time.sleep(0.1)
+    content = pyperclip.paste()
 
-        driver.get(url)
-        time.sleep(2)
+    time.sleep(uniform(1, 2))
+    return content
 
-        try:
-            accept_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Accept all cookies') or contains(text(), 'Accept All Cookies') or contains(text(), 'I Accept') or contains(text(), 'Accept Cookies')]")
-            accept_button.click()
-        except NoSuchElementException:
-            print("no cookies button found")
-
-        body = driver.find_element("tag name", "body")
-        body.send_keys(Keys.CONTROL, 'a')
-        time.sleep(0.2)
-
-        body.send_keys(Keys.CONTROL, 'c')
-        time.sleep(0.1)      
-      
-        content = driver.execute_script("return navigator.clipboard.readText();")
-        
-        return content
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-    finally:
-        driver.quit()
-
-def process_with_chat_gpt():
-    pass
+def process_with_openai_api(content):
+    #TODO
+    return ("abstract-text", "yes/no")
 
 if __name__ == "__main__":
     path = Path(__file__).parent
-
-    scraped_htmls_dir = scrape_search_results(path=path)
-    urls = gather_urls(path=scraped_htmls_dir)
-    for url in urls:
-        print(url)
-    print(len(urls))
-    directory = Path(path / "page-contents")
-    directory.mkdir(exist_ok=True)
-
-    '''driver_path = r'C:\webdrivers\chromedriver.exe'
+    driver_path = r'C:\webdrivers\chromedriver.exe' #to sie powinno sczytywac skąds, np z pliku tekstowego
     service = Service(driver_path)
     driver = webdriver.Chrome(service=service)
-    for index, url in enumerate(urls):
-        content = get_contents(url=url, driver=driver)[:10000]
-        with open(Path(path / "page-contents" / f"contents{index + 1}.txt"), 'w', encoding='utf-8') as file:
-            file.write(content)'''
+    scraped_htmls_dir = Path(path / "scraped-htmls")
+    scraped_htmls_dir.mkdir(exist_ok=True)
+    page_contents_dir = Path(path / "page-contents")
+    page_contents_dir.mkdir(exist_ok=True)
+    results_df = pd.DataFrame(columns=["abstract", "if_relevant"])
+
+    next_page = 0
+    user_input_query = input("search query for google scholar:\n")
+    while True:
+        scraped_html = scrape_search_results(driver=driver,
+                                            query=user_input_query,
+                                            page_numer=str(next_page))
+        next_page += 1
+        urls = gather_urls(scraped_html=scraped_html)
+        if len(urls) == 0:
+            break
+        for index, url in enumerate(urls):
+            content = get_contents(url=url, driver=driver)[:10000]
+            abstract, if_relevant = process_with_openai_api(content=content)
+            results_df.loc[len(results_df)] = [abstract, if_relevant]
+    
+    driver.quit()
+    no_dups_results_df = results_df.drop_duplicates(subset="abstract")
+    sorted_no_dups_results_df = no_dups_results_df.sort_values(by=["if_relevant", "abstract"],
+                                               ascending=[False, True])
+    sorted_no_dups_results_df.to_csv("results.csv", index=False)
--- try to copy links.py
+++ try to copy links.py
@@ -1,21 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import time
 
 driver_path = r'C:\webdrivers\chromedriver.exe'
 service = Service(driver_path) 
 driver = webdriver.Chrome(service=service)
 
 # Load the webpage
-url = "https://scholar.google.com/scholar?hl=pl&as_sdt=0%2C5&q=hallucinogens&btnG=&oq=ha"
+url = "https://www.pw.edu.pl"
 driver.get(url)
-input()
-
+time.sleep(2)
+url = "https://www.example.com"
+driver.get(url)
+time.sleep(2)
 # Get the full HTML source code of the page
-html_source = driver.page_source
+#html_source = driver.page_source
 
 # Optionally, save the HTML to a file
-with open("page_source.html", "w", encoding="utf-8") as file:
-    file.write(html_source)
+'''with open("page_source.html", "w", encoding="utf-8") as file:
+    file.write(html_source)'''
 
 # Close the browser when done
 driver.quit()
